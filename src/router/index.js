4c1225c43d7e67fbada8852d05cf0bf45b1cfcd0
nankaNULL <534129087@qq.com>
2019-02-26 11:59:59 +0800

    路由修改

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,31 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import Home from '@/page/home';
-import List from '@/page/list';
+import { Router, Route } from 'react-router-dom';
+import createHistory from "history/createBrowserHistory";
+export const history = createHistory();
+import Home from '@/pages/home';
+import List from '@/pages/list';
 
-export const Routers = class extends React.Component{
-  render(){
-    <Router>
-      <div>
-        <Route exact path="/" component={Home}></Route>
-        <Route path="/list" component={List}></Route>
-      </div>
-    </Router>
+export const Routers = class extends React.Component {
+  render() {
+    return (
+      <Router history={history}>
+        <div>
+          <Route exact path="/" component={Home}></Route>
+          <Route path="/list" component={List}></Route>
+        </div>
+      </Router>
+    );
   }
 }
+// export const Routers = class extends React.Component{
+//   render(){
+//     return (
+//       <Router>
+//         <div>
+//           <Route exact path="/" component={Home}></Route>
+//           <Route path="/list" component={List}></Route>
+//         </div>
+//       </Router>
+//     )
+//   }
+// }
