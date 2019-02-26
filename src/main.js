58f1339fb46f385822a46247aab528677903abc1
nankaNULL <534129087@qq.com>
2019-02-26 11:59:49 +0800

    删除备注

--- src/main.js
+++ src/main.js
@@ -2,11 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Routers } from './router'
 
-// export default class Main extends React.Component{
-//   render(){
-//     return <div></div>
-//   }
-// }
 const render = Component => 
   ReactDOM.render(
     <Component />,
