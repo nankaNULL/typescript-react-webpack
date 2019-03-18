688f624449628e2d5e13d31b2f5634328dbdf174
nankaNULL <534129087@qq.com>
2019-03-18 14:24:23 +0800

    provider 将store注入

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import { Routers } from './router'
+import store from './store'
 
 const render = Component => 
   ReactDOM.render(
-    <Component />,
+    <Provider store={store}>
+      <Component />
+    </Provider>,
     document.getElementById('root')
   )
 render(Routers)
