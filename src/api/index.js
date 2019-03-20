277cd8c44f1193967f6c5d469e87bae3cb2cbf88
nankaNULL <534129087@qq.com>
2019-03-20 15:28:31 +0800

    整理文件存放位置

--- src/api/index.js
+++ src/api/index.js
@@ -1,11 +1,7 @@
 import _ from 'lodash';
-import http from './http';
+import http from '@/utils/http';
 import API_URL from './url';
-// export default{
-//   getTheData(params){
-//     return http.get('https://api.github.com/users/chriscoyier/repos', params)
-//   },
-// }
+
 function mapUrlObjToFuncObj(urlObj){
   let API = {};
   // _.keys(urlObj)得到一个拿到所有对象名的数组，然后对这个数组遍历，key就是对象名
