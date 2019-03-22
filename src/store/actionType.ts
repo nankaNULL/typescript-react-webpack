4d1fefa20a63c91d5298dd1cc7eb7d789f6515e3
nankaNULL <534129087@qq.com>
2019-03-22 15:57:44 +0800

    修改actionType生成方式

--- src/store/actionType.ts
+++ src/store/actionType.ts
@@ -1,8 +1,10 @@
-// import mirror from 'mirror-creator';
-// export const bookAction = mirror([
-//   'BOOK_MENU_LIST'
-// ],'global/')
-
-export const bookAction = {
-  "BOOK_MENU_LIST":"BOOK_MENU_LIST"
+function createActionType(arrayNames: Array<string>) {
+  let actionTypeObj: any = {};
+  arrayNames.forEach((item: string) => {
+    actionTypeObj[item] = item;
+  })
+  return actionTypeObj;
 }
+export const bookAction = createActionType([
+  'BOOK_MENU_LIST'
+])
