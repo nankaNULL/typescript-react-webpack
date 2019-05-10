2dfcbafd4ca187c19ee49c79a645937969e697b3
nankaNULL <534129087@qq.com>
2019-05-10 16:19:52 +0800

    配置全局样式

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { Routers } from './router'
-import store from './store'
+import { Routers } from './router';
+import store from './store';
+import './scss/index.scss'
 
 const render = Component => 
   ReactDOM.render(
