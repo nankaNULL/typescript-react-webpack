acb6147a020630f722d473398251a693d8fa40ab
nankaNULL <534129087@qq.com>
2019-05-14 11:23:02 +0800

    electron桌面应用

---
+++ app.js
@@ -0,0 +1,110 @@
+// Modules to control application life and create native browser window
+const electron = require('electron')
+const {app, BrowserWindow, Menu, globalShortcut} = electron
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let mainWindow
+var template = [{
+  label: '编辑1',
+  submenu: [{
+    label: '撤销',
+    accelerator: 'CmdOrCtrl+Z',
+    // role: 'undo',
+    click: function () {
+      mainWindow.loadURL('http://localhost:8181/list')
+      console.log("??")
+    }
+  }, {
+    label: '重做',
+    accelerator: 'Shift+CmdOrCtrl+Z',
+    role: 'redo'
+  }, {
+    type: 'separator'
+  }, {
+    label: '复制',
+    accelerator: 'CmdOrCtrl+C',
+    role: 'copy'
+  }, {
+    label: '粘贴',
+    accelerator: 'CmdOrCtrl+V',
+    role: 'paste'
+  }]
+}, {
+  label: '帮助',
+  role: 'help',
+  submenu: [{
+    label: '学习更多',
+    click: function () {
+      electron.shell.openExternal('http://electron.atom.io')
+    }
+  },{
+    label: '切换全屏',
+    accelerator: (function () {
+      if (process.platform === 'darwin') {
+        return 'Ctrl+Command+F'
+      } else {
+        return 'F12'
+      }
+    })(),
+    click: function (item, focusedWindow) {
+      if (focusedWindow) {
+        focusedWindow.setFullScreen(!focusedWindow.isFullScreen())
+      }
+    }
+  }]
+}];
+
+function createWindow () {
+  // Create the browser window.
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+
+  // menu
+  const menu = Menu.buildFromTemplate(template)
+  Menu.setApplicationMenu(menu)
+  // and load the index.html of the app.
+  // mainWindow.loadFile('index.html')
+  mainWindow.loadURL('http://localhost:8181')
+
+  // Open the DevTools.
+  mainWindow.webContents.openDevTools()
+
+  // Emitted when the window is closed.
+  mainWindow.on('closed', function () {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    mainWindow = null
+  })
+
+  // globalShortcut.register('CmdOrCtrl+P', () => {
+  //   console.log('CommandOrControl+X is pressed')
+  // })
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', createWindow)
+
+// Quit when all windows are closed.
+app.on('window-all-closed', function () {
+  // On macOS it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') app.quit()
+})
+
+app.on('activate', function () {
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (mainWindow === null) createWindow()
+})
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.
