e928eb108b23d77a3066e44dfedba2f81a75dc2f
nankanull <534129087@qq.com>
2019-10-25 16:37:53 +0800

    登录模块

--- src/api/url.ts
+++ src/api/url.ts
@@ -1,10 +1,37 @@
 export default {
-  getTheData:{
-    method:'get',
-    url:'https://api.github.com/users/chriscoyier/repos'
+  getTheData: {
+    method: 'get',
+    url: 'https://api.github.com/users/chriscoyier/repos'
   },
-  getErrData:{
-    method:'get',
-    url:'https://api.github.com/users/chrissycoyier/repos'
+  getErrData: {
+    method: 'get',
+    url: 'https://api.github.com/users/chrissycoyier/repos'
+  },
+
+  /**
+   * 用户登录
+   */
+
+  // 登录
+  userLogin: {
+    method: 'post',
+    url: '/api/user/login'
+  },
+
+  // 获取用户信息
+  getUserInfo: {
+    method: 'get',
+    url: '/api/user/info'
+  },
+
+  getToken: {
+    method: 'get',
+    url: '/api/user/token'
+  },
+
+  // 登出
+  userLogout: {
+    method: 'post',
+    url: '/api/user/logout'
   }
 }
--- src/store/globalReducer.ts
+++ src/store/globalReducer.ts
@@ -1,7 +1,63 @@
-const initState = {
-  globalTest: 'globalTest'
+import { createActionType } from '@/store/actionType';
+import { message } from 'antd';
+const actionType = createActionType([
+  'USER_LOGIN',
+  'GET_USER_INFO'
+])
+
+const initialState = {
+  user_token: '',
+  userInfo: {}
 }
 
-export const globalReducer = (state = initState) => {
-  return state;
+export const globalActions = {
+  // 登录
+  userLogin: (params: any) => async (dispatch: any, getState: void, { API }: any) => {
+    API.userLogin(params).then((res: any) => {
+      const { result, result_message, data } = res;
+      if (!result) {
+        return message.error(result_message)
+      }
+      message.success(result_message);
+      /* token */
+      if (data) {
+        dispatch({
+          type: actionType.USER_LOGIN,
+          payload: res.data.token
+        });
+      }
+    })
+  },
+
+  // 获取用户信息
+  fetchUserInfo: () => async (dispatch: any, getState: void, { API }: any) => {
+    API.getUserInfo().then((res: any) => {
+      const { result, result_message, data } = res;
+      if (!result) {
+        return message.error(result_message)
+      }
+      dispatch({
+        type: actionType.GET_USER_INFO,
+        payload: data
+      });
+    })
+  },
+}
+
+
+export const globalReducer = (state = initialState, action: any) => {
+  const { type, payload } = action;
+  switch (type) {
+    case actionType.USER_LOGIN:
+      return Object.assign({}, state, {
+        user_token: payload
+      });
+    case actionType.GET_USER_INFO:{
+      return Object.assign({}, state, {
+        userInfo: payload
+      });
+    }
+
+    default: return state;
+  }
 }
